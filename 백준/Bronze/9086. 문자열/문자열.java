7d320c8bfa864767d9cf34be63402afd572972db
minsik0 <167049108+minsik0@users.noreply.github.com>
2026-02-05 18:23:56 +0900

    [Bronze V] Title: 문자열, Time: 164 ms, Memory: 17652 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int T = sc.nextInt();
+        for(int i = 0; i < T; i++){
+            String str = sc.next();
+            System.out.print(str.charAt(0));
+            System.out.println(str.charAt(str.length() -1));
+        }
+    }
+}
