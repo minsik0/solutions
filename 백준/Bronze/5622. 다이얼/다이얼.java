32ab5b0db182831caa66f5721fc3b574f0951aeb
minsik0 <167049108+minsik0@users.noreply.github.com>
2026-02-07 01:47:58 +0900

    [Bronze II] Title: 다이얼, Time: 164 ms, Memory: 17644 KB -BaekjoonHub

---
+++ 백준/Bronze/5622. 다이얼/다이얼.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        String s = sc.next();
+        int time = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            switch (ch) {
+                case 'A': case 'B': case 'C':
+                    time += 3; 
+                    break;
+                case 'D': case 'E': case 'F':
+                    time += 4; 
+                    break;
+                case 'G': case 'H': case 'I':
+                    time += 5;
+                    break;
+                case 'J': case 'K': case 'L':
+                    time += 6; 
+                    break;
+                case 'M': case 'N': case 'O':
+                    time += 7; 
+                    break;
+                case 'P': case 'Q': case 'R': case 'S':
+                    time += 8; 
+                    break;
+                case 'T': case 'U': case 'V':
+                    time += 9; 
+                    break;
+                case 'W': case 'X': case 'Y': case 'Z':
+                    time += 10; 
+                    break;
+            }
+        }
+        System.out.println(time);
+    }
+}
