c85e15ff02cebf46aa802cec15805d4c1cdff5f0
minsik0 <167049108+minsik0@users.noreply.github.com>
2026-02-07 14:23:00 +0900

    [Bronze V] Title: 사칙연산, Time: 156 ms, Memory: 17696 KB -BaekjoonHub

--- 백준/Bronze/10869. 사칙연산/사칙연산.java
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -5,13 +5,11 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		int A = sc.nextInt();
 		int B = sc.nextInt();
- 
-        sc.close();
- 
-		System.out.println(A+B);
-		System.out.println(A-B);
-		System.out.println(A*B);
-		System.out.println(A/B);
-		System.out.println(A%B);
-	}
+        
+        System.out.println(A+B);
+        System.out.println(A-B);
+        System.out.println(A*B);
+        System.out.println(A/B);
+        System.out.println(A%B);
+    }
 }
