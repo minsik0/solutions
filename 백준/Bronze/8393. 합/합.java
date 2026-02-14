dc90f60360c7e8aefc912860871c734dcb27f9ca
minsik0 <167049108+minsik0@users.noreply.github.com>
2026-02-14 15:52:58 +0900

    [Bronze V] Title: 합, Time: 96 ms, Memory: 14056 KB -BaekjoonHub

--- 백준/Bronze/8393. 합/합.java
+++ 백준/Bronze/8393. 합/합.java
@@ -1,16 +1,16 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int n = sc.nextInt();
-        int s = 0;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+        int N = Integer.parseInt(br.readLine());
+        int sum = 0;
         
-        for(int i=0; i <= n; i++){
-            s += i;  
+        for(int i = 1; i <= N; i++){
+            sum += i;
         }
-            System.out.println(s);
-    }
+        System.out.println(sum);
+	}
 }
