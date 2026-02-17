34b5b60f119a5aff55aea176a785ea70e4617c5a
minsik0 <167049108+minsik0@users.noreply.github.com>
2026-02-17 17:45:24 +0900

    [Bronze IV] Title: 더하기, Time: 104 ms, Memory: 14028 KB -BaekjoonHub

--- 백준/Bronze/9085. 더하기/더하기.java
+++ 백준/Bronze/9085. 더하기/더하기.java
@@ -6,18 +6,18 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         int T = Integer.parseInt(br.readLine());
-
-        for (int i = 0; i < T; i++) {
+        
+        for(int i = 0; i < T; i++){
             int N = Integer.parseInt(br.readLine());
             
             StringTokenizer st = new StringTokenizer(br.readLine());
             int sum = 0;
-
-            for (int j = 0; j < N; j++) {
+            
+            for(int j = 0; j < N; j++){
                 sum += Integer.parseInt(st.nextToken());
             }
             sb.append(sum).append("\n");
         }
-        System.out.print(sb);
+        System.out.println(sb);
     }
 }
