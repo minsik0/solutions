5bf5f863afd67940562fcbaf043f6ace22c26614
minsik0 <167049108+minsik0@users.noreply.github.com>
2026-02-17 18:14:35 +0900

    [Bronze IV] Title: 과자, Time: 100 ms, Memory: 14172 KB -BaekjoonHub

---
+++ 백준/Bronze/10156. 과자/과자.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int K = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int price = K * N;
+        
+        if (M >= price) {
+            System.out.println(0);
+        } else {
+            System.out.println(price - M);
+        }
+    }
+}
