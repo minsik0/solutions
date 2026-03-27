fff159ee0966a423182019656a37da739545a8f0
minsik0 <167049108+minsik0@users.noreply.github.com>
2026-03-27 21:35:37 +0900

    [level 0] Title: 이어 붙인 수, Time: 15.13 ms, Memory: 87.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(int[] num_list) {
+        String odd = "";
+        String even = "";
+    
+        
+        for(int i = 0; i < num_list.length; i++){
+            if(num_list[i] % 2 == 0){
+                even += num_list[i]; 
+                
+            }else if(num_list[i] % 2 != 0){
+                odd += num_list[i];
+            }
+        }
+        int oddNum = Integer.parseInt(odd);
+        int evenNum = Integer.parseInt(even);
+        
+        return oddNum + evenNum;
+    }
+}
