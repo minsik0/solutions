830891d76e46178530b09fd27943cf43c317ad8b
minsik0 <167049108+minsik0@users.noreply.github.com>
2026-05-10 18:13:04 +0900

    [level 0] Title: 0 떼기, Time: 0.03 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String solution(String n_str) {
+        String answer = "";
+        int n = Integer.parseInt(n_str);
+        
+        return answer = String.valueOf(n);
+    }
+}
