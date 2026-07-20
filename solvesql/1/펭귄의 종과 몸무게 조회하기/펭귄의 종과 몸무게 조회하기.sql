b87c572eb84483ff4f51846073fcedc6180112c5
minsik0 <qwew1212@naver.com>
2026-07-20 19:36:03 +0900

    [난이도1]Title: 펭귄의 종과 몸무게 조회하기

---
+++ solvesql/1/펭귄의 종과 몸무게 조회하기/펭귄의 종과 몸무게 조회하기.sql
@@ -0,0 +1,6 @@
+SELECT species, body_mass_g
+FROM penguins
+WHERE species IS NOT NULL AND body_mass_g IS NOT NULL
+ORDER BY body_mass_g DESC, species ASC
+
+--https://solvesql.com/problems/species-and-mass-of-penguins/
