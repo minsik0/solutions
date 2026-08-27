bdea49c4f6c087fd0c598caed2d32ebae4d4c3ab
minsik0 <167049108+minsik0@users.noreply.github.com>
2026-08-27 12:36:25 +0900

    [level 0] Title: 수열과 구간 쿼리 4, Time: 1.33 ms, Memory: 76.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        for (int[] query : queries) {
+            int s = query[0];
+            int e = query[1];
+            int k = query[2];
+            
+            for (int i = s; i <= e; i++) {
+                if (k != 0 && i % k == 0) {
+                    arr[i] += 1;
+                } else if (k == 0 && i == 0) {
+                    
+                }
+            }
+        }
+        return arr;
+    }
+}
