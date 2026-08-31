6d21f3a7e31d148f13b820e4794bc4fa4f46a2e4
minsik0 <167049108+minsik0@users.noreply.github.com>
2026-08-31 21:02:39 +0900

    [level 0] Title: 주사위 게임 3, Time: 3.14 ms, Memory: 85.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int a, int b, int c, int d) {
+        int[] dice = {a, b, c, d};
+        Arrays.sort(dice);
+
+        if (dice[0] == dice[3]) {
+            return 1111 * dice[0];
+        }
+
+        if (dice[0] == dice[2] || dice[1] == dice[3]) {
+            int p = dice[1]; 
+            int q = (dice[0] == dice[2]) ? dice[3] : dice[0];
+            return (10 * p + q) * (10 * p + q);
+        }
+
+        if (dice[0] == dice[1] && dice[2] == dice[3]) {
+            return (dice[0] + dice[2]) * Math.abs(dice[0] - dice[2]);
+        }
+
+        if (dice[0] == dice[1]) {
+            return dice[2] * dice[3];
+        } else if (dice[1] == dice[2]) {
+            return dice[0] * dice[3];
+        } else if (dice[2] == dice[3]) {
+            return dice[0] * dice[1];
+        }
+
+        return dice[0];
+    }
+}
